09329226ba89621058d160ff0692d25524cb09d3
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-15 21:05:45 +0530

    skill card animated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import * as motion from "motion/react-client";
 
 const Card = ({ image, name }) => {
     return (
-        <div className='flex w-[130px] h-[150px] items-center justify-center text-center flex-col border-2 p-8 rounded mt-10'>
+        <motion.div
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.8 }}
+            className='flex w-[130px] h-[150px] items-center justify-center text-center flex-col border-2 p-8 rounded mt-10'>
             <img src={image} alt="skill-icon" />
             <h3 className='font-medium'>{name}</h3>
-        </div>
+        </motion.div>
     )
 }
 
