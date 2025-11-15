b4656747fdfbc03fbf96df446b4f31f94aeb327c
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-15 16:17:01 +0530

    social media added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
+import SocialMedia from './components/SocialMedia';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <SocialMedia />
     </>
   )
 }
---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const SocialMedia = () => {
+  return (
+    <div className='lg:mx-15 mx-8 lg:-mt-15 mb-7 -mt-5'>
+        <ul className='flex gap-5 justify-center lg:justify-start'>
+            <li className='border-2 p-1.5 rounded cursor-pointer hover:bg-gray-100 transform transition-all'>
+                <img src="linkedin.svg" className='lg:w-6 md:w-6 w-4' alt="linkedin" />
+            </li>
+            <li className='border-2 p-1.5 rounded cursor-pointer hover:bg-gray-100 transform transition-all'>
+                <img src="github.svg" className='lg:w-6 md:w-6 w-4' alt="github" />
+            </li>
+            <li className='border-2 p-1.5 rounded cursor-pointer hover:bg-gray-100 transform transition-all'>
+                <img src="twitter.svg" className='lg:w-6 md:w-6 w-4' alt="twitter" />
+            </li>
+            <li className='border-2 p-1.5 rounded cursor-pointer hover:bg-gray-100 transform transition-all'>
+                <img src="discort.svg" className='lg:w-6 md:w-6 w-4' alt="discort" />
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default SocialMedia;
