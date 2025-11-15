0d4c75fb2bbfc7b74c147f3db58af836f114fa05
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-15 19:43:41 +0530

    icon and logo update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div className='flex justify-between lg:mx-15 mx-8 my-3 items-center'>
-        <img src="/Logo.png" alt="logo" />
+        <img src="/Logo.png" className='w-[120px]' alt="logo" />
         <div className='w-1/3 md:w-75 hidden lg:block md:block'>
             <ul className='flex justify-between font-bold'>
                 <li className='hover:opacity-70 transition-all cursor-pointer'>About Me</li>
