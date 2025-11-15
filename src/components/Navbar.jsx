89efb196dbc49ab094906d4d54edbc509bb430ae
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-15 14:01:41 +0530

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between mx-10 my-3 items-center'>
+        <img src="/Logo.png" alt="logo" />
+        <div className='w-1/3'>
+            <ul className='flex justify-between font-bold'>
+                <li>About Me</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+        <div>
+            <Button variant="contained">Resume &nbsp; <i class="fa-solid fa-download"></i></Button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar;
