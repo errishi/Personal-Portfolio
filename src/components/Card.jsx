38208c80f905c131fdaa9f5d614d02f780714e92
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-15 17:36:25 +0530

    skill section added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ image, name }) => {
     return (
-        <div className='flex w-[130px] h-[150px] items-center flex-col border-2 p-8 rounded mt-10'>
+        <div className='flex w-[130px] h-[150px] items-center justify-center text-center flex-col border-2 p-8 rounded mt-10'>
             <img src={image} alt="skill-icon" />
             <h3 className='font-medium'>{name}</h3>
         </div>
--- src/components/SkillSection.jsx
+++ src/components/SkillSection.jsx
@@ -3,11 +3,21 @@ import Card from './Card';
 
 const SkillSection = () => {
   return (
-    <div className='lg:mx-15 mx-8 my-4'>
+    <div className='lg:mx-15 mx-8 my-8 mt-20'>
         <h1 className='font-bold text-3xl text-center'>My Skills</h1>
-        <div className='flex justify-evenly lg:justify-start gap-10 md:justify-start'>
-            <Card image={"/icon-git.svg"} name={"Git"} />
+        <div className='flex justify-evenly lg:justify-center gap-x-12 md:justify-start flex-wrap'>
+            <Card image={"/icon-html.svg"} name={"HTML"} />
+            <Card image={"/icon-css.svg"} name={"CSS"} />
             <Card image={"/icon-javscript.svg"} name={"JavaScript"} />
+            <Card image={"/icon-bootstrap.svg"} name={"Bootstrap"} />
+            <Card image={"/icon-tailwindcss.png"} name={"Tailwind CSS"} />
+            <Card image={"/icon-react.svg"} name={"React Js"} />
+            <Card image={"/icon-node.svg"} name={"Node Js"} />
+            <Card image={"/icon-mongodb.png"} name={"MongoDB"} />
+            <Card image={"/expressjs.png"} name={"Express Js"} />
+            <Card image={"/icon-java.svg"} name={"Java"} />
+            <Card image={"/icon-python.svg"} name={"Python"} />
+            <Card image={"/icon-git.svg"} name={"Git"} />
         </div>
     </div>
   )
