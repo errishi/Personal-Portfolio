2a74de158cc34b4f7990a75221612d5a5df0f1fc
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-11-16 13:26:35 +0530

    smooth scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,33 @@ import AboutSection from './components/AboutSection';
 import ProjectSection from './components/ProjectSection';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
+import ReactLenis from 'lenis/react';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <SocialMedia />
-      <SkillSection />
-      <AboutSection />
-      <ProjectSection />
-      <ContactForm />
-      <Footer />
+      <ReactLenis
+      root
+      options={{
+        lerp: 0.1,      //smooth factor
+        duration: 1.2,    //animation duration
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        smoothTouch: false,         //keep mobile touch normal
+        touchMultiplier: 2
+      }}
+      >
+        <Navbar />
+        <HeroSection />
+        <SocialMedia />
+        <SkillSection />
+        <AboutSection />
+        <ProjectSection />
+        <ContactForm />
+        <Footer />
+      </ReactLenis>
     </>
   )
 }
