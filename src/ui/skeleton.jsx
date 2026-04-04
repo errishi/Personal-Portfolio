e0ce4c2d80474ce2140cd95f0ad418ade2a07aa8
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-04-05 01:21:06 +0530

    ui change

--- src/ui/skeleton.jsx
+++
@@ -1,9 +0,0 @@
-const Skeleton = ({ className = "" }) => {
-  return (
-    <div
-      className={`bg-gray-200 dark:bg-gray-700 animate-pulse rounded-md ${className}`}
-    />
-  )
-}
-
-export { Skeleton }
